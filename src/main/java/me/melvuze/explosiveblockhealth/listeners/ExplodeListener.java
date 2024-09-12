2d3bbe2b9658bf8ba7d29a09560bee3cc0e10bfb
Leopold95 <leopold9586@gmail.com>
2024-09-12 11:29:53 +0300

    updates

--- src/main/java/me/melvuze/explosiveblockhealth/listeners/ExplodeListener.java
+++ src/main/java/me/melvuze/explosiveblockhealth/listeners/ExplodeListener.java
@@ -4,11 +4,13 @@ import com.jeff_media.customblockdata.CustomBlockData;
 import me.melvuze.explosiveblockhealth.ExplosiveBlockHealth;
 import me.melvuze.explosiveblockhealth.core.Config;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
 
 public class ExplodeListener implements Listener {
@@ -37,21 +39,26 @@ public class ExplodeListener implements Listener {
                 for (int z = startZ; z <= endZ; z++) {
                     Block block = location.getWorld().getBlockAt(x, y, z);
 
-                    //plugin.getLogger().warning("no block data 1");
-
                     if(!plugin.getExplosiveBlocks().containsKey(block.getType()))
                         continue;
-
-                    //plugin.getLogger().warning("no block data 2");
 
                     CustomBlockData data = new CustomBlockData(block, plugin);
                     if (data.has(plugin.getHealthKey())) {
                         int health = data.get(plugin.getHealthKey(), PersistentDataType.INTEGER);
 
-                        plugin.getLogger().warning(String.valueOf(health));
+                        //plugin.getLogger().warning(String.valueOf(health));
 
-                        if (health == 1){
-                            block.breakNaturally();
+                        if (health <= 1){
+                            Material oldType = block.getType();
+
+                            block.setType(Material.AIR);
+
+                            if(Config.getBoolean("should-drop")){
+                                block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(oldType));
+                            }
+
+                            data.remove(plugin.getHealthKey());
+                            continue;
                         }
 
                         health--;
@@ -59,7 +66,6 @@ public class ExplodeListener implements Listener {
                         data.set(plugin.getHealthKey(), PersistentDataType.INTEGER, health);
                     }
                     else {
-                        //plugin.getLogger().warning("no block data");
                         int health = plugin.getExplosiveBlocks().get(block.getType());
                         data.set(plugin.getHealthKey(), PersistentDataType.INTEGER, health);
                     }
